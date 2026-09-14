07222b4d3051e0d25de3c48ca16ddcd37640ee99
Thoomas-nrv <thomasmorvan082@gmail.com>
2026-09-14 10:18:36 +0200

    ça beuge

--- SDL.cpp
+++ SDL.cpp
@@ -6,8 +6,8 @@ int main() {
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
 
-    SDL_Init(SDL_INIT_EVERYTHING)
-    SDL_CreateWindowAndRenderer(640,480,0,&window,&renderer);
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
