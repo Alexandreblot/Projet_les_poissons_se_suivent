dce8014d4e94b9804d1d62277a513d01bd553af6
Alexandreblot <alexandre.blot@isen-ouest.yncrea.fr>
2026-09-14 10:02:37 +0200

    Test2

--- SDL.cpp
+++ SDL.cpp
@@ -1,65 +1,21 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <iostream>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
 
-int main(int argc, char* argv[]) {
-    // 1. Initialisation de la SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "Erreur d'initialisation de la SDL : " << SDL_GetError() << std::endl;
-        return 1;
-    }
+int main() {
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
 
-    // 2. Création de la fenêtre
-    SDL_Window* window = SDL_CreateWindow(
-        "Ma premiere fenetre SDL en C++",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN
-    );
+    SDL_Init(SDL_INIT_EVERYTHING)
+    SDL_CreateWindowAndRenderer(640,480,0,&window,&renderer);
 
-    if (!window) {
-        std::cerr << "Erreur de création de la fenêtre : " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-    // 3. Création du rendu (renderer)
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        std::cerr << "Erreur de création du renderer : " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, 320, 240);
 
-    // 4. Boucle principale du jeu / application
-    bool running = true;
-    SDL_Event event;
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
 
-    while (running) {
-        // Gestion des événements (clavier, croix de la fenêtre, etc.)
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-
-        // Effacer l'écran en couleur (ex: bleu)
-        SDL_SetRenderDrawColor(renderer, 30, 144, 255, 255);
-        SDL_RenderClear(renderer);
-
-        // Afficher le résultat à l'écran
-        SDL_RenderPresent(renderer);
-    }
-
-    // 5. Nettoyage et fermeture
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
