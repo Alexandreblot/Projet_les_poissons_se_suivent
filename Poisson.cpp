e0a2280e673917c5c5285f1c7cafc76f8bd67e8b
Alexandre Blot <alexandre.blot@isen-ouest.yncrea.fr>
2026-09-14 11:51:00 +0200

    2eme essaie

--- Poisson.cpp
+++ Poisson.cpp
@@ -3,3 +3,20 @@
 #include <SDL2/SDL.h>
 
 #include <iostream>
+
+Poisson::Poisson(SDL_Renderer* renderer, PoissonType type)
+    : renderer(renderer), type(type), vitesse(0.0), direction(0.0), positionX(0.0), positionY(0.0) {
+    loadTextures();
+}
+
+Poisson::~Poisson() {
+    for (auto texture : textures) {
+        if (texture) {
+            SDL_DestroyTexture(texture);
+        }
+    }
+}
+
+Poisson::PoissonType Poisson::getType() const {
+    return type;
+}
--- Poisson.hpp
+++ Poisson.hpp
@@ -12,21 +12,31 @@ public:
         Bleu
     };
 
+    PoissonType getType() const;
+
     Poisson(SDL_Renderer* renderer, PoissonType type);
     ~Poisson();
 
     SDL_Texture* getTexture() const {
         return textures[static_cast<int>(type)];
     }
-private:
-    SDL_Renderer* renderer;
-    PoissonType type;
-    std::array<SDL_Texture*, 3> textures;
+
     double vitesse;
     double direction;
     double positionX;
     double positionY;
 
+private:
+    SDL_Renderer* renderer;
+    PoissonType type;
+    std::array<SDL_Texture*, 3> textures;
+
     void loadTextures();
+
+    void updatePosition();
+
+    void render() const;
+
+    void handleCollisionWithWindowBounds();
 
 };
